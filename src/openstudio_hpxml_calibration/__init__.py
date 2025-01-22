dcef523030a4d8b1830ab1db7e4ffabcf70bb608
Noel Merket <noel.merket@nrel.gov>
2025-01-22 15:19:19 -0700

    very simple cli

--- src/openstudio_hpxml_calibration/__init__.py
+++ src/openstudio_hpxml_calibration/__init__.py
@@ -1,2 +1,15 @@
-def main() -> None:
-    print("Hello from openstudio-hpxml-calibration!")
+import subprocess
+import click
+
+
+@click.group()
+def cli() -> None:
+    pass
+
+
+@cli.command()
+def openstudio_version() -> None:
+    resp = subprocess.run(
+        ["openstudio", "openstudio_version"], capture_output=True, check=True
+    )
+    print(resp.stdout.decode())
