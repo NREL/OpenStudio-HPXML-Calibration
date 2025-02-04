d61f38fa1a02d541f2ed725020fdc1e82a4f63e2
Nathan Moore <nathan.moore@nrel.gov>
2025-02-04 16:42:34 -0700

    even simpler initial test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,7 +13,13 @@
 from openstudio_hpxml_calibration import app
 
 
-def test_greet(capsys):
-    app(["openstudio-version"])
+def test_cli_has_help(capsys):
+    app(["--help"])
     captured = capsys.readouterr()
-    assert "HPXML v4.0" in captured.out
+    assert "Return the OpenStudio-HPXML" in captured.out
+
+
+# def test_cli_calls_openstudio(capsys):
+#     app(["openstudio-version"])
+#     captured = capsys.readouterr()
+#     assert "HPXML v4.0" in captured.out
