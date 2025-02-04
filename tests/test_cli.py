79fe9787810d5229989c9a17b7ebdb91835494a1
Nathan Moore <nathan.moore@nrel.gov>
2025-02-04 16:53:15 -0700

    Set up basic infrastructure (#13)
    * add .pre-commit config file
    
    * don't enforce a specific version of python for everyone
    
    * allow large files to be added to a commit
    
    * don't use mypy in pre-commit
    
    * add pre-commit & pytest as dev dependencies
    
    * gitignore python-version
    
    * linting config files
    
    * readme with developer instructions
    
    * add ci workflow with GitHub Actions
    
    * kinda dummy test to have something for pytest to run in CI
    
    * skip some linting in test files
    
    * improve test to call cli
    
    * adding .vscode to .gitignore
    
    * udpate readme instruction for calling pytest
    
    * even simpler initial test
    
    ---------
    
    Co-authored-by: Noel Merket <noel.merket@nrel.gov>

---
+++ tests/test_cli.py
@@ -0,0 +1,25 @@
+# @pytest.fixture
+# def setup_data():
+#     # Setup phase
+#     data = {"key": "value"}
+#     print("\nSetting up resources...")
+#     yield data  # Provide data to the test
+#     # Teardown phase
+#     print("\nTearing down resources...")
+
+# def test_example(setup_data):
+#     assert setup_data["key"] == "value"
+
+from openstudio_hpxml_calibration import app
+
+
+def test_cli_has_help(capsys):
+    app(["--help"])
+    captured = capsys.readouterr()
+    assert "Return the OpenStudio-HPXML" in captured.out
+
+
+# def test_cli_calls_openstudio(capsys):
+#     app(["openstudio-version"])
+#     captured = capsys.readouterr()
+#     assert "HPXML v4.0" in captured.out
