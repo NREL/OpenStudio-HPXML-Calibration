0e89bd8ae002b8e5f943619ea91178e61f627ee0
Nathan Moore <nathan.moore@nrel.gov>
2025-02-07 10:55:12 -0700

    limit choices for appropriate parameters with Enum

--- src/openstudio_hpxml_calibration/__init__.py
+++ src/openstudio_hpxml_calibration/__init__.py
@@ -1,16 +1,31 @@
 import subprocess
+from enum import Enum
 from importlib.metadata import version
 from pathlib import Path
 
 from cyclopts import App
+
+OSHPXML_PATH = Path(__file__).resolve().parent.parent / "OpenStudio-HPXML"
+
+
+class Granularity(str, Enum):
+    HOURLY = "hourly"
+    DAILY = "daily"
+    MONTHLY = "monthly"
+
+
+class Format(str, Enum):
+    CSV = "csv"
+    JSON = "json"
+    MSGPACK = "msgpack"
+    CSV_DVIEW = "csv_dview"
+
 
 app = App(
     version=version("openstudio-hpxml-calibration"),
     version_flags=["--version", "-v"],
     help="Calibrate an HPXML model to provided utility data using OpenStudio-HPXML",
 )
-
-oshpxml_path = Path(__file__).resolve().parent.parent / "OpenStudio-HPXML"
 
 
 @app.command
@@ -19,7 +34,7 @@ def openstudio_version() -> None:
     resp = subprocess.run(
         [
             "openstudio",
-            str(oshpxml_path / "workflow" / "run_simulation.rb"),
+            str(OSHPXML_PATH / "workflow" / "run_simulation.rb"),
             "--version",
         ],
         capture_output=True,
@@ -29,7 +44,12 @@ def openstudio_version() -> None:
 
 
 @app.command
-def run_sim(hpxml_filepath, output_format=None, output_dir=None, granularity=None) -> None:
+def run_sim(
+    hpxml_filepath: str,
+    output_format: Format | None = None,
+    output_dir: str | None = None,
+    granularity: Granularity | None = None,
+) -> None:
     """Simulate an HPXML file using the OpenStudio-HPXML workflow
 
     Parameters
@@ -37,18 +57,18 @@ def run_sim(hpxml_filepath, output_format=None, output_dir=None, granularity=Non
     hpxml_filepath: str
         Path to the HPXML file to simulate
     output_format: str
-        Output file format type (csv, json, msgpack, csv_dview). Default is csv
+        Output file format type. Default is csv.
     output_dir: str
-        Output directory to save simulation results dir. Default is HPXML file dir
+        Output directory to save simulation results dir. Default is HPXML file dir.
     granularity: str
-        Granularity of simulation results (hourly, daily, monthly). Default is annual
+        Granularity of simulation results. Default is annual.
     """
-    run_simulation_command = ["openstudio", str(oshpxml_path / "workflow" / "run_simulation.rb"), "--xml", hpxml_filepath]
+    run_simulation_command = ["openstudio", str(OSHPXML_PATH / "workflow" / "run_simulation.rb"), "--xml", hpxml_filepath]
     if granularity is not None:
-        granularity = [f"--{granularity}", "ALL"]
+        granularity = [f"--{granularity.value}", "ALL"]
         run_simulation_command.extend(granularity)
     if output_format is not None:
-        output_format = ["--output-format", output_format]
+        output_format = ["--output-format", output_format.value]
         run_simulation_command.extend(output_format)
     if output_dir is not None:
         output_dir = ["--output-dir", output_dir]
