443c783c0d6fedc6190a73dc4ed6d04d2fbfaa8c
Scott Horowitz <scott.horowitz@nrel.gov>
2025-03-17 15:55:13 -0600

    Trim trailing whitespace

--- src/measures/ModifyXML/tests/modify_xml_test.rb
+++ src/measures/ModifyXML/tests/modify_xml_test.rb
@@ -24,25 +24,25 @@ class ModifyXMLTest < Minitest::Test
     args_hash['save_file_path'] = @tmp_hpxml_path
     args_hash['heating_setpoint_offset'] = -1.5
     args_hash['cooling_setpoint_offset'] = 2.5
-    
+
     hpxml_bldg = _test_measure(args_hash)
-    
+
     # Check for expected change
     assert_equal(66.5, hpxml_bldg.hvac_controls[0].heating_setpoint_temp)
     assert_equal(80.5, hpxml_bldg.hvac_controls[0].cooling_setpoint_temp)
   end
-  
+
   def _test_measure(args_hash)
     # create an instance of the measure
     measure = ModifyXML.new
-    
+
     runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
     model = OpenStudio::Model::Model.new
-    
+
     # get arguments
     arguments = measure.arguments(model)
     argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
-    
+
     # populate argument with specified hash value if specified
     arguments.each do |arg|
       temp_arg_var = arg.clone
@@ -51,17 +51,17 @@ class ModifyXMLTest < Minitest::Test
       end
       argument_map[arg.name] = temp_arg_var
     end
-    
+
     # run the measure
     measure.run(model, runner, argument_map)
     result = runner.result
-    
+
     # show the output
     show_output(result) unless result.value.valueName == 'Success'
-    
+
     # assert that it ran correctly
     assert_equal('Success', result.value.valueName)
-    
+
     # return new HPXML building
     hpxml = HPXML.new(hpxml_path: args_hash['save_file_path'])
     return hpxml.buildings[0]
