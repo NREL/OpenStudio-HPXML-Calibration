b7e59a48d1be07ba191d2b857412aa67ff65d225
Nathan Moore <nathan.moore@nrel.gov>
2025-03-18 13:10:42 -0600

    new test for hourly weekday & weekend setpoint modification

--- src/measures/ModifyXML/tests/modify_xml_test.rb
+++ src/measures/ModifyXML/tests/modify_xml_test.rb
@@ -25,11 +25,35 @@ class ModifyXMLTest < Minitest::Test
     args_hash['heating_setpoint_offset'] = -1.5
     args_hash['cooling_setpoint_offset'] = 2.5
 
+    original_bldg = HPXML.new(hpxml_path: args_hash['xml_file']).buildings[0]
     hpxml_bldg = _test_measure(args_hash)
 
     # Check for expected change
-    assert_equal(66.5, hpxml_bldg.hvac_controls[0].heating_setpoint_temp)
-    assert_equal(80.5, hpxml_bldg.hvac_controls[0].cooling_setpoint_temp)
+    new_heating_setpoint = original_bldg.hvac_controls[0].heating_setpoint_temp + args_hash['heating_setpoint_offset'] # 66.5
+    new_cooling_setpoint = original_bldg.hvac_controls[0].cooling_setpoint_temp + args_hash['cooling_setpoint_offset'] # 80.5
+    assert_equal(new_heating_setpoint, hpxml_bldg.hvac_controls[0].heating_setpoint_temp)
+    assert_equal(new_cooling_setpoint, hpxml_bldg.hvac_controls[0].cooling_setpoint_temp)
+  end
+
+  def test_hourly_setpoints
+    # create hash of argument values.
+    args_hash = {}
+    args_hash['xml_file'] = File.join(@oshpxml_root_path, 'workflow', 'sample_files', 'base-hvac-setpoints-daily-schedules.xml')
+    args_hash['save_file_path'] = @tmp_hpxml_path
+    args_hash['heating_setpoint_offset'] = -1.5
+    args_hash['cooling_setpoint_offset'] = 2.5
+
+    hpxml_bldg = _test_measure(args_hash)
+
+    # Check for expected change
+    new_weekday_heating_setpoints = '62.5, 62.5, 62.5, 62.5, 62.5, 62.5, 62.5, 68.5, 68.5, 64.5, 64.5, 64.5, 64.5, 64.5, 64.5, 64.5, 64.5, 66.5, 66.5, 66.5, 66.5, 66.5, 62.5, 62.5'
+    new_weekend_heating_setpoints = '66.5, 66.5, 66.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5, 68.5'
+    new_weekday_cooling_setpoints = '78.5, 78.5, 78.5, 78.5, 78.5, 78.5, 78.5, 73.5, 73.5, 78.5, 78.5, 78.5, 78.5, 78.5, 78.5, 78.5, 78.5, 76.5, 76.5, 76.5, 76.5, 76.5, 78.5, 78.5'
+    new_weekend_cooling_setpoints = '76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5, 76.5'
+    assert_equal(new_weekday_heating_setpoints, hpxml_bldg.hvac_controls[0].weekday_heating_setpoints)
+    assert_equal(new_weekend_heating_setpoints, hpxml_bldg.hvac_controls[0].weekend_heating_setpoints)
+    assert_equal(new_weekday_cooling_setpoints, hpxml_bldg.hvac_controls[0].weekday_cooling_setpoints)
+    assert_equal(new_weekend_cooling_setpoints, hpxml_bldg.hvac_controls[0].weekend_cooling_setpoints)
   end
 
   def _test_measure(args_hash)
