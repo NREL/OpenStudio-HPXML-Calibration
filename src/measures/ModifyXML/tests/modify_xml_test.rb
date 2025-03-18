3b277eddf3264f17850f4b3b043d8734054a876b
Scott Horowitz <scott.horowitz@nrel.gov>
2025-03-18 10:46:37 -0600

    Stub unit tests for Ruby Measure (#30)
    * Add example unit test for measure, attempt to add to CI.
    
    * Trim trailing whitespace
    
    * Will this work on Windows too?

--- src/measures/ModifyXML/tests/modify_xml_test.rb
+++ src/measures/ModifyXML/tests/modify_xml_test.rb
@@ -8,93 +8,40 @@ require 'fileutils'
 require_relative '../measure'
 
 class ModifyXMLTest < Minitest::Test
-  # def setup
-  # end
-
-  # def teardown
-  # end
-
-  def test_number_of_arguments_and_argument_names
-    # create an instance of the measure
-    measure = ModifyXML.new
-
-    # make an empty model
-    model = OpenStudio::Model::Model.new
-
-    # get arguments and test that they are what we are expecting
-    arguments = measure.arguments(model)
-    assert_equal(1, arguments.size)
-    assert_equal('space_name', arguments[0].name)
+  def setup
+    @oshpxml_root_path = File.absolute_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'OpenStudio-HPXML'))
+    @tmp_hpxml_path = File.join(File.dirname(__FILE__), 'tmp.xml')
   end
 
-  def test_bad_argument_values
-    # create an instance of the measure
-    measure = ModifyXML.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
-
-    # make an empty model
-    model = OpenStudio::Model::Model.new
-
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
-
-    # create hash of argument values
-    args_hash = {}
-    args_hash['space_name'] = ''
-
-    # populate argument with specified hash value if specified
-    arguments.each do |arg|
-      temp_arg_var = arg.clone
-      if args_hash.key?(arg.name)
-        assert(temp_arg_var.setValue(args_hash[arg.name]))
-      end
-      argument_map[arg.name] = temp_arg_var
-    end
-
-    # run the measure
-    measure.run(model, runner, argument_map)
-    result = runner.result
-
-    # show the output
-    show_output(result)
-
-    # assert that it did fail
-    assert_equal('Fail', result.value.valueName)
-    assert_equal(1, result.stepErrors.size)
-    assert_match(/empty space name/i, result.stepErrors.first)
+  def teardown
+    File.delete(@tmp_hpxml_path) if File.exist? @tmp_hpxml_path
   end
 
-  def test_good_argument_values
-    # create an instance of the measure
-    measure = ModifyXML.new
-
-    # create runner with empty OSW
-    osw = OpenStudio::WorkflowJSON.new
-    runner = OpenStudio::Measure::OSRunner.new(osw)
-
-    # load the test model
-    translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = "#{File.dirname(__FILE__)}/example_model.osm"
-    model = translator.loadModel(path)
-    refute_empty(model)
-    model = model.get
-
-    # store the number of spaces in the seed model
-    num_spaces_seed = model.getSpaces.size
-
-    # get arguments
-    arguments = measure.arguments(model)
-    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
-
+  def test_setpoint_offsets
     # create hash of argument values.
-    # If the argument has a default that you want to use, you don't need it in the hash
     args_hash = {}
-    args_hash['space_name'] = 'New Space'
-    # using defaults values from measure.rb for other arguments
+    args_hash['xml_file'] = File.join(@oshpxml_root_path, 'workflow', 'sample_files', 'base.xml')
+    args_hash['save_file_path'] = @tmp_hpxml_path
+    args_hash['heating_setpoint_offset'] = -1.5
+    args_hash['cooling_setpoint_offset'] = 2.5
+
+    hpxml_bldg = _test_measure(args_hash)
+
+    # Check for expected change
+    assert_equal(66.5, hpxml_bldg.hvac_controls[0].heating_setpoint_temp)
+    assert_equal(80.5, hpxml_bldg.hvac_controls[0].cooling_setpoint_temp)
+  end
+
+  def _test_measure(args_hash)
+    # create an instance of the measure
+    measure = ModifyXML.new
+
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+    model = OpenStudio::Model::Model.new
+
+    # get arguments
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
 
     # populate argument with specified hash value if specified
     arguments.each do |arg|
@@ -105,33 +52,18 @@ class ModifyXMLTest < Minitest::Test
       argument_map[arg.name] = temp_arg_var
     end
 
-    # Ensure the model did not start with a space named like requested
-    refute_includes(model.getSpaces.map(&:nameString), "New Space")
-
     # run the measure
     measure.run(model, runner, argument_map)
     result = runner.result
 
     # show the output
-    show_output(result)
+    show_output(result) unless result.value.valueName == 'Success'
 
     # assert that it ran correctly
     assert_equal('Success', result.value.valueName)
-    assert_equal(1, result.stepInfo.size)
-    assert_empty(result.stepWarnings)
 
-    # check that there is now 1 extra space
-    assert_equal(1, model.getSpaces.size - num_spaces_seed)
-    assert_includes(model.getSpaces.map(&:nameString), "New Space")
-
-    refute_empty(result.stepInitialCondition)
-    assert_equal('The building started with 4 spaces.', result.stepInitialCondition.get)
-
-    refute_empty(result.stepFinalCondition)
-    assert_equal('The building finished with 5 spaces.', result.stepFinalCondition.get)
-
-    # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}/output/test_output.osm"
-    model.save(output_file_path, true)
+    # return new HPXML building
+    hpxml = HPXML.new(hpxml_path: args_hash['save_file_path'])
+    return hpxml.buildings[0]
   end
 end
