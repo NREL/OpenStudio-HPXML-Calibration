77d35ed74bfa79f3692b97133e2719348815c8db
Scott Horowitz <scott.horowitz@nrel.gov>
2025-04-21 17:50:18 -0600

    format

--- tests/test_hpxmls.py
+++ tests/test_hpxmls.py
@@ -4,7 +4,9 @@ import pytest
 
 from openstudio_hpxml_calibration.hpxml import HpxmlDoc
 
-real_home_hpxmls = list((pathlib.Path(__file__).resolve().parent.parent / "test_hpxmls" / "real_homes").glob("*.xml"))
+real_home_hpxmls = list(
+    (pathlib.Path(__file__).resolve().parent.parent / "test_hpxmls" / "real_homes").glob("*.xml")
+)
 
 
 @pytest.mark.parametrize("filename", real_home_hpxmls, ids=lambda x: x.stem)
