49ebecd71f4940c9438ae7e8395966133a9c0ba4
Nathan Moore <nathan.moore@nrel.gov>
2025-04-29 11:18:31 -0600

    test calibration on same building as cli, using pytest fixture

--- tests/test_calibrate.py
+++ tests/test_calibrate.py
@@ -1,29 +1,37 @@
-# import json
+import json
 from pathlib import Path
 
 # from shutil import rmtree
-# import pytest
+import pytest
+
 from openstudio_hpxml_calibration.calibrate import Calibrate
 
 TEST_DIR = Path(__file__).parent
 TEST_DATA_DIR = TEST_DIR / "data"
-# USER_CONFIG = TEST_DATA_DIR / "user_config.json"
-
-# @pytest.fixture
-# def test_data():
-# Setup phase
-# data: dict = json.loads(USER_CONFIG.read_text())
-# return data  # Provide data dict to the test
-# Teardown phase
+TEST_CONFIG = TEST_DATA_DIR / "test_config.json"
 
 
-def test_calibrate_reads_hpxml():
-    foo = Calibrate("test_hpxmls/real_homes/house21.xml")
+@pytest.fixture
+def test_data():
+    # Setup phase
+    data: dict = json.loads(TEST_CONFIG.read_text())
+    return data  # Provide data dict to the test
+    # Teardown phase
+
+
+def test_calibrate_reads_hpxml(test_data):
+    foo = Calibrate(test_data["sample_xml_file"])
     normalized_usage = foo.normalize_bills()
-    for fuel_type, (normalized_heating_usage, normalized_cooling_usage) in normalized_usage.items():
+    for fuel_type, (
+        normalized_heating_usage,
+        normalized_cooling_usage,
+        normalized_baseload_usage,
+    ) in normalized_usage.items():
         assert normalized_heating_usage is not None
         assert normalized_cooling_usage is not None
-        # print(f"Fuel Type: {fuel_type}")
-        # print(f"Normalized Heating Usage: {normalized_heating_usage}")
-        # print(f"Normalized Cooling Usage: {normalized_cooling_usage}")
+        assert normalized_baseload_usage is not None
+    #     print(f"Fuel Type: {fuel_type}")
+    #     print(f"Normalized Heating Usage: {normalized_heating_usage}")
+    #     print(f"Normalized Cooling Usage: {normalized_cooling_usage}")
+    #     print(f"Normalized Baseload Usage: {normalized_baseload_usage}")
     # assert False is True
