16e797d55702cdc81eb0753ae4a210d312047d23
Nathan Moore <nathan.moore@nrel.gov>
2025-05-27 15:44:32 -0600

    update test assertion for new rounding value

--- tests/test_calibrate.py
+++ tests/test_calibrate.py
@@ -16,12 +16,14 @@ def test_data():
     # Setup phase
     data: dict = json.loads(TEST_CONFIG.read_text())
     return data  # Provide data dict to the test
-    # Teardown phase
+    # To implement a teardown phase:
+    # yield data
+    # put any teardown code here if needed
 
 
 def test_calibrate_normalizes_bills_to_weather(test_data) -> None:
-    foo = Calibrate(original_hpxml_filepath=test_data["sample_xml_file"])
-    normalized_usage = foo.get_normalized_consumption_per_bill()
+    cal = Calibrate(original_hpxml_filepath=test_data["sample_xml_file"])
+    normalized_usage = cal.get_normalized_consumption_per_bill()
     for fuel_type, normalized_consumption in normalized_usage.items():
         # print(fuel_type)
         # print(normalized_consumption)
@@ -29,11 +31,12 @@ def test_calibrate_normalizes_bills_to_weather(test_data) -> None:
         # Assert that baseload has 12 non-zero values
         assert not pd.isna(normalized_consumption["baseload"]).any()
         # assert False is True
+        # this bogus assert creates a test failure so the print statements can be seen for debugging
 
 
 def test_get_model_results(test_data) -> None:
-    foo = Calibrate(original_hpxml_filepath=test_data["sample_xml_file"])
-    simulation_results = foo.get_model_results(
+    cal = Calibrate(original_hpxml_filepath=test_data["sample_xml_file"])
+    simulation_results = cal.get_model_results(
         json_results_path=Path(test_data["annual_json_results_path"])
     )
     for fuel_type, disagg_results in simulation_results.items():
@@ -43,17 +46,21 @@ def test_get_model_results(test_data) -> None:
         # print(disagg_results)
         assert [s for s in disagg_results if "heating" in s]
         # assert False is True
+        # this bogus assert creates a test failure so the print statements can be seen for debugging
 
 
 def test_compare_results(test_data):
-    foo = Calibrate(original_hpxml_filepath=test_data["sample_xml_file"])
-    normalized_usage = foo.get_normalized_consumption_per_bill()
-    simulation_results = foo.get_model_results(
+    cal = Calibrate(original_hpxml_filepath=test_data["sample_xml_file"])
+    normalized_usage = cal.get_normalized_consumption_per_bill()
+    simulation_results = cal.get_model_results(
         json_results_path=Path(test_data["annual_json_results_path"])
     )
-    foo.compare_results(
+    comparison = cal.compare_results(
         normalized_consumption=normalized_usage, annual_model_results=simulation_results
     )
+    assert len(comparison) == 2  # Should have two fuel types in the comparison
+    assert comparison["electricity"]["Absolute Error"]["baseload"] == 1918.2
+    assert comparison["natural gas"]["Bias Error"]["heating"] == -125.3
     # for fuel_type, comparison in results_comparison.items():
     #     print(fuel_type)
     #     print(comparison["model_results"].keys())
@@ -65,3 +72,4 @@ def test_compare_results(test_data):
     #     print(comparison["model_results"]["(136, 165)"].keys())
     # print(comparison["model_results"]["(158, 187)"])
     # assert False is True
+    # this bogus assert creates a test failure so the print statements can be seen for debugging
