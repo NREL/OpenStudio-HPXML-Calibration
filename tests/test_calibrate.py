6e4d5b13f6efdcf2eee9d2678830f935edb6cf62
Nathan Moore <nathan.moore@nrel.gov>
2025-06-04 16:39:51 -0600

    actually read output in calibration tests

--- tests/test_calibrate.py
+++ tests/test_calibrate.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pandas as pd
 import pytest
+from loguru import logger
 
 from openstudio_hpxml_calibration.calibrate import Calibrate
 
@@ -26,13 +27,13 @@ def test_calibrate_normalizes_bills_to_weather(test_data) -> None:
     cal = Calibrate(original_hpxml_filepath=test_data["sample_xml_file"])
     normalized_usage = cal.get_normalized_consumption_per_bill()
     for fuel_type, normalized_consumption in normalized_usage.items():
-        # print(fuel_type)
-        # print(normalized_consumption)
         assert normalized_consumption.shape == (12, 5)
         # Assert that baseload has 12 non-zero values
         assert not pd.isna(normalized_consumption["baseload"]).any()
-        # assert False is True
-        # this bogus assert creates a test failure so the print statements can be seen for debugging
+        if fuel_type == "electricity":
+            assert normalized_consumption["baseload"].sum().round(3) == 20.222
+        elif fuel_type == "natural gas":
+            assert normalized_consumption["baseload"].sum().round(3) == 17.854
 
 
 def test_get_model_results(test_data) -> None:
@@ -41,13 +42,17 @@ def test_get_model_results(test_data) -> None:
         json_results_path=Path(test_data["annual_json_results_path"])
     )
     for fuel_type, disagg_results in simulation_results.items():
-        # assert that this string is one of the keys in the disaggregated results
-        # print(fuel_type)
-        # print(len(disagg_results))
-        # print(disagg_results)
-        assert [s for s in disagg_results if "heating" in s]
-        # assert False is True
-        # this bogus assert creates a test failure so the print statements can be seen for debugging
+        if fuel_type == "electricity":
+            assert disagg_results["heating"] == 3.372
+            assert disagg_results["cooling"] == 8.907
+            assert disagg_results["baseload"] == 26.745
+        elif fuel_type == "natural gas":
+            assert disagg_results["heating"] == 151.884
+            assert disagg_results["baseload"] == 26.682
+        elif disagg_results["baseload"] != 0.0:
+            logger.warning(
+                f"Unexpected fuel type {fuel_type} with non-zero baseload: {disagg_results['baseload']}"
+            )
 
 
 def test_compare_results(test_data):
@@ -59,6 +64,6 @@ def test_compare_results(test_data):
     comparison = cal.compare_results(
         normalized_consumption=normalized_usage, annual_model_results=simulation_results
     )
-    assert len(comparison) == 2  # Should have two fuel types in the comparison
+    assert len(comparison) == 2  # Should have two fuel types in the comparison for this building
     assert comparison["electricity"]["Absolute Error"]["baseload"] == 1918.2
     assert comparison["natural gas"]["Bias Error"]["heating"] == -125.3
