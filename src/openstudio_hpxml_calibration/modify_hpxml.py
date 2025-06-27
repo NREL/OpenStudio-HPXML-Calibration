b944d5e2ae8f4294298070269a1b56c32ad3e70b
Nathan Moore <nathan.moore@nrel.gov>
2025-06-27 10:20:02 -0600

    convert datetime columns of bill csv, and save xml to file for run-sim

--- src/openstudio_hpxml_calibration/modify_hpxml.py
+++ src/openstudio_hpxml_calibration/modify_hpxml.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pandas as pd
 from loguru import logger
-from lxml import objectify
+from lxml import etree, objectify
 from lxml.builder import ElementMaker
 
 from openstudio_hpxml_calibration.hpxml import HpxmlDoc
@@ -19,6 +19,11 @@ def set_consumption_on_hpxml(hpxml_object: HpxmlDoc, csv_bills_filepath: Path) -
     """Add bills from csv to hpxml object"""
 
     bills = pd.read_csv(csv_bills_filepath)
+    # Convert to datetimes, and include the final day of the bill period
+    bills["StartDateTime"] = pd.to_datetime(bills["StartDateTime"])
+    bills["EndDateTime"] = pd.to_datetime(bills["EndDateTime"]) + pd.Timedelta(days=1)
+    bills["StartDateTime"] = bills["StartDateTime"].dt.strftime("%Y-%m-%dT%H:%M:%S")
+    bills["EndDateTime"] = bills["EndDateTime"].dt.strftime("%Y-%m-%dT%H:%M:%S")
 
     # Set up xml objects to hold the bill data
     consumption_details = E.ConsumptionDetails()
@@ -57,4 +62,7 @@ def set_consumption_on_hpxml(hpxml_object: HpxmlDoc, csv_bills_filepath: Path) -
         consumption_details.append(new_obj)
 
     hpxml_object.root.append(consumption_section)
+    et = etree.ElementTree(hpxml_object.root)
+    et.write("model_with_consumption.xml", pretty_print=True)
+
     return hpxml_object
