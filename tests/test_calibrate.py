cfa7febd8ca917ac806a8918ad9df381b6bff09b
Nathan Moore <nathan.moore@nrel.gov>
2025-07-30 11:48:47 -0600

    set test order and update test values for new OS-HPXML version

--- tests/test_calibrate.py
+++ tests/test_calibrate.py
@@ -41,6 +41,7 @@ def test_calibrate_normalizes_bills_to_weather(test_data) -> None:
             assert normalized_consumption["baseload"].sum().round(3) == pytest.approx(21.711, 0.005)
 
 
+@pytest.mark.order(2)
 def test_get_model_results(test_data) -> None:
     cal = Calibrate(original_hpxml_filepath=test_data["sample_xml_file"])
     if results_path.exists():
@@ -52,17 +53,18 @@ def test_get_model_results(test_data) -> None:
         )
     for fuel_type, disagg_results in simulation_results.items():
         if fuel_type == "electricity":
-            assert disagg_results["cooling"] == 8.907
-            assert disagg_results["baseload"] == 26.745
+            assert disagg_results["cooling"] == 9.329
+            assert disagg_results["baseload"] == 26.67
         elif fuel_type == "natural gas":
-            assert disagg_results["heating"] == 151.884
-            assert disagg_results["baseload"] == 26.682
+            assert disagg_results["heating"] == 151.671
+            assert disagg_results["baseload"] == 26.349
         elif disagg_results["baseload"] != 0.0:
             logger.warning(
                 f"Unexpected fuel type {fuel_type} with non-zero baseload: {disagg_results['baseload']}"
             )
 
 
+@pytest.mark.order(3)
 def test_compare_results(test_data):
     cal = Calibrate(original_hpxml_filepath=test_data["sample_xml_file"])
     normalized_usage = cal.get_normalized_consumption_per_bill()
@@ -77,8 +79,8 @@ def test_compare_results(test_data):
         normalized_consumption=normalized_usage, annual_model_results=simulation_results
     )
     assert len(comparison) == 2  # Should have two fuel types in the comparison for this building
-    assert comparison["electricity"]["Absolute Error"]["baseload"] == 1566.5
-    assert comparison["natural gas"]["Bias Error"]["heating"] == -79.5
+    assert comparison["electricity"]["Absolute Error"]["baseload"] == 1544.5
+    assert comparison["natural gas"]["Bias Error"]["heating"] == -79.3
 
 
 def test_add_bills(test_data):
