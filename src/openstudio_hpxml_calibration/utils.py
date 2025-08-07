859d5b650ec7422d24619239872baa8561d66fd8
Nathan Moore <nathan.moore@nrel.gov>
2025-08-07 15:33:39 -0600

    refactor private functions to utils.py

--- src/openstudio_hpxml_calibration/utils.py
+++ src/openstudio_hpxml_calibration/utils.py
@@ -3,6 +3,8 @@ import os
 from pathlib import Path
 
 import platformdirs
+import yaml
+from loguru import logger
 
 OS_HPXML_PATH = Path(__file__).resolve().parent.parent / "OpenStudio-HPXML"
 
@@ -20,3 +22,29 @@ def calculate_sha256(filepath: os.PathLike, block_size: int = 65536):
         for byte_block in iter(lambda: f.read(block_size), b""):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
+
+
+def _merge_with_defaults(user_config, default_config: dict) -> dict:
+    """Merge default values into user's config"""
+    if not isinstance(user_config, dict):
+        return user_config
+    merged = default_config.copy()
+    for key, val in user_config.items():
+        if key in merged and isinstance(merged[key], dict):
+            merged[key] = _merge_with_defaults(val, merged[key])
+        else:
+            merged[key] = val
+    return merged
+
+
+def _load_config(config_filepath: Path | None = None) -> dict:
+    default_config_filepath = Path(__file__).resolve().parent / "default_ga_config.yaml"
+    with open(default_config_filepath) as f:
+        default_config = yaml.safe_load(f)
+    if not config_filepath or not Path(config_filepath).exists():
+        logger.info(f"Config file {config_filepath} not found. Using default configuration.")
+        return default_config
+    else:
+        with open(config_filepath) as f:
+            config = yaml.safe_load(f)
+        return _merge_with_defaults(config, default_config)
