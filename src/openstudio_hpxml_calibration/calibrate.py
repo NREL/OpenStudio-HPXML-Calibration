eae03aa87d08ff9706bafdbfbfd822c1c006e79f
Nathan Moore <nathan.moore@nrel.gov>
2025-08-26 12:29:06 -0600

    refactor degree_days methods to their own module

--- src/openstudio_hpxml_calibration/calibrate.py
+++ src/openstudio_hpxml_calibration/calibrate.py
@@ -16,12 +16,14 @@ from deap import algorithms, base, creator, tools
 from loguru import logger
 from pathos.multiprocessing import ProcessingPool as Pool
 
-import openstudio_hpxml_calibration.weather_normalization.utility_data as ud
 from openstudio_hpxml_calibration import app
 from openstudio_hpxml_calibration.hpxml import FuelType, HpxmlDoc
 from openstudio_hpxml_calibration.modify_hpxml import set_consumption_on_hpxml
 from openstudio_hpxml_calibration.units import convert_units
 from openstudio_hpxml_calibration.utils import _load_config
+from openstudio_hpxml_calibration.weather_normalization.degree_days import (
+    calculate_annual_degree_days,
+)
 from openstudio_hpxml_calibration.weather_normalization.inverse_model import InverseModel
 
 # Ensure the creator is only created once
@@ -238,75 +240,10 @@ class Calibrate:
 
         return comparison_results
 
-    def calculate_annual_degree_days(self) -> dict[str, float]:
-        """Calculate annual heating and cooling degree days from TMY data and actual weather data.
-
-        Returns:
-            dict: A dictionary containing annual heating and cooling degree days for TMY weather data.
-            dict: A dictionary containing annual heating and cooling degree days for actual weather data.
-        """
-        tmy_dry_bulb_temps_f = ud.calc_daily_dbs(self.hpxml).f
-        bills_by_fuel_type, _, _ = ud.get_bills_from_hpxml(self.hpxml)
-        lat, lon = self.hpxml.get_lat_lon()
-        bill_tmy_degree_days = {}
-        total_period_actual_dd = {}
-
-        # Use day-of-year because TMY data contains multiple years
-        tmy_temp_index_doy = tmy_dry_bulb_temps_f.index.dayofyear
-
-        for fuel_type, bills in bills_by_fuel_type.items():
-            if fuel_type not in (
-                FuelType.FUEL_OIL,
-                FuelType.PROPANE,
-                FuelType.WOOD,
-                FuelType.WOOD_PELLETS,
-            ):
-                continue  # Skip fuels that are not delivered fuels
-            # format fuel type for dictionary keys
-            fuel_type_name = fuel_type.name.lower().replace("_", " ")
-            # Get degree days of actual weather during bill periods
-            _, actual_temp_f = ud.join_bills_weather(bills, lat, lon)
-            daily_actual_temps = actual_temp_f.resample("D").mean()
-            actual_degree_days = ud.calc_heat_cool_degree_days(daily_actual_temps)
-            actual_degree_days = {k: round(v) for k, v in actual_degree_days.items()}
-            total_period_actual_dd[fuel_type_name] = actual_degree_days
-
-            # Get degree days of TMY weather
-            bill_results = []
-            for _, row in bills.iterrows():
-                start_doy = row["start_day_of_year"]
-                end_doy = row["end_day_of_year"]
-
-                # Handle bills that wrap around the end of the year
-                if start_doy <= end_doy:
-                    mask = (tmy_temp_index_doy >= start_doy) & (tmy_temp_index_doy <= end_doy)
-                else:
-                    mask = (tmy_temp_index_doy >= start_doy) | (tmy_temp_index_doy <= end_doy)
-
-                # Select the dry bulb temperatures for the bill period
-                bill_dry_bulbs_tmy = tmy_dry_bulb_temps_f[mask]
-                tmy_degree_days = ud.calc_heat_cool_degree_days(bill_dry_bulbs_tmy)
-                bill_results.append(
-                    {
-                        "start_date": row["start_date"],
-                        "end_date": row["end_date"],
-                        **tmy_degree_days,
-                    }
-                )
-            bill_tmy_degree_days[fuel_type_name] = bill_results
-
-        total_period_tmy_dd = {}
-        for fuel, bill_list in bill_tmy_degree_days.items():
-            hdd_total = round(sum(bill.get("HDD65F", 0) for bill in bill_list))
-            cdd_total = round(sum(bill.get("CDD65F", 0) for bill in bill_list))
-            total_period_tmy_dd[fuel] = {"HDD65F": hdd_total, "CDD65F": cdd_total}
-
-        return total_period_tmy_dd, total_period_actual_dd
-
     def simplified_annual_usage(
         self, model_results: dict, delivered_consumption, fuel_type: str
     ) -> dict:
-        total_period_tmy_dd, total_period_actual_dd = self.calculate_annual_degree_days()
+        total_period_tmy_dd, total_period_actual_dd = calculate_annual_degree_days(self.hpxml)
 
         comparison_results = {}
 
