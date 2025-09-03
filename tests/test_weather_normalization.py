bb7c258611b60a60f9cbe6741e92b1207c071468
Nathan Moore <nathan.moore@nrel.gov>
2025-09-03 11:41:02 -0600

    restore flaky on test_fit_model

--- tests/test_weather_normalization.py
+++ tests/test_weather_normalization.py
@@ -170,7 +170,7 @@ def test_curve_fit(results_dir, filename):
 
 
 # Use flaky to address intermittent tcl errors on Windows https://stackoverflow.com/questions/71443540/intermittent-pytest-failures-complaining-about-missing-tcl-files-even-though-the
-# @pytest.mark.flaky(max_runs=3)
+@pytest.mark.flaky(max_runs=3)
 # Skipping because of this bug in Python https://github.com/python/cpython/issues/125235#issuecomment-2412948604
 @pytest.mark.skipif(
     sys.platform == "win32"
