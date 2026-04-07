be8d6ecaab37c9d517457c7f89cf579402758e9d
bpark1327 <59635239+bpark1327@users.noreply.github.com>
2026-04-07 15:52:44 -0600

    Improve HPXML generation in consumption data processing (#134)
    * Improve HPXML generation in consumption data processing
    
    * Revert back isd_station selection
    
    ---------
    
    Co-authored-by: Scott Horowitz <scott.horowitz@nrel.gov>

--- src/openstudio_hpxml_calibration/modify_hpxml.py
+++ src/openstudio_hpxml_calibration/modify_hpxml.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
 import pandas as pd
-from loguru import logger
 from lxml import objectify
-from lxml.builder import ElementMaker
+from lxml.objectify import ElementMaker
 
 from openstudio_hpxml_calibration.hpxml import HpxmlDoc
 
@@ -17,7 +16,8 @@ def set_consumption_on_hpxml(hpxml_object: HpxmlDoc, csv_bills_filepath: Path) -
     E = ElementMaker(namespace=NS, nsmap=NSMAP)
 
     bills = pd.read_csv(csv_bills_filepath)
-    # Convert to datetimes, and include the final day of the bill period
+
+    # Datetime handling
     bills["StartDateTime"] = pd.to_datetime(bills["StartDateTime"], format="mixed")
     bills["EndDateTime"] = pd.to_datetime(bills["EndDateTime"], format="mixed") + pd.Timedelta(
         days=1
@@ -33,35 +33,37 @@ def set_consumption_on_hpxml(hpxml_object: HpxmlDoc, csv_bills_filepath: Path) -
         consumption_details,
     )
 
-    # separate bill data by fuel type, then remove fuel type info
-    dfs_by_fuel = {}
-    for f_type in bills["FuelType"].unique():
-        fuel_data = bills.loc[bills["FuelType"] == f_type]
-        dfs_by_fuel[f_type] = fuel_data.drop(["FuelType"], axis=1)
+    # Separate bill data by fuel type, then remove fuel type info
+    for fuel in bills["FuelType"].unique():
+        consumption_df = bills.loc[bills["FuelType"] == fuel].drop(["FuelType"], axis=1)
 
-    # Turn the dfs of bills into xml objects that match hpxml schema
-    for fuel, consumption_df in dfs_by_fuel.items():
-        # Grab the unit of measure from the first row, then drop it from the dataframe because it
-        # doesn't get added to every consumption section in the xml object.
         unit = consumption_df["UnitofMeasure"].iloc[0]
-        narrower_consumption_df = consumption_df.drop(["UnitofMeasure"], axis=1)
-        # logger.debug(f"{fuel=}")
-        xml_str = narrower_consumption_df.to_xml(
+        narrower_df = consumption_df.drop(["UnitofMeasure"], axis=1)
+
+        if unit is None:
+            raise ValueError(f"Unsupported fuel type: {fuel}")
+
+        xml_str = narrower_df.to_xml(
             root_name="ConsumptionInfo",
             row_name="ConsumptionDetail",
             index=False,
             xml_declaration=False,
             namespaces=NSMAP,
         )
-        new_obj = objectify.fromstring(xml_str)
 
-        if unit is None:
-            logger.error(f"Unsupported fuel type: {fuel}")
+        new_obj = objectify.fromstring(xml_str.encode())
 
-        consumption_type = E.ConsumptionType(E.Energy(E.FuelType(fuel), E.UnitofMeasure(unit)))
+        consumption_type = E.ConsumptionType(
+            E.Energy(
+                E.FuelType(fuel),
+                E.UnitofMeasure(unit),
+            )
+        )
+
         new_obj.insert(0, consumption_type)
         new_obj.insert(0, E.UtilityID())
         consumption_details.append(new_obj)
 
     hpxml_object.root.append(consumption_section)
+
     return hpxml_object
